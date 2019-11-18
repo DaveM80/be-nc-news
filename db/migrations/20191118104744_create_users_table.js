84271f01f7f88046b835bab87ab60de8420770a0
DaveM880 <eldeividas@gmail.com>
2019-11-18 11:31:07 +0000

    migrations setup

---
+++ db/migrations/20191118104744_create_users_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  console.log("Creating users table")
+  return knex.schema
+  .createTable("users", usersTable =>{
+      usersTable.string("username").primary();
+      usersTable.string("avatar_url");
+      usersTable.string("name").notNullable();
+  })
+};
+
+exports.down = function(knex) {
+    console.log("Dropping users table");
+    return knex.schema.dropTable("users");
+
+};
